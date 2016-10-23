b86ce632770c753391a9de3ce69797b0eeecd628
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-10-23 18:46:17 +0200

    Version number

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ Plugin URI: #
 Text Domain: mhm-attachment-from-ftp
 Author: Mark Howells-Mead
 Author URI: https://permanenttourist.ch/
-Version: 0.4.0
+Version: 0.4.1
 */
 
 namespace MHM\WordPress\AttachmentFromFtp;
